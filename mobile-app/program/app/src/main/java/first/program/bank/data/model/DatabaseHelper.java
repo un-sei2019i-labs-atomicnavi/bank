56cca63677729bbdb0f33c4cb4a66bb5ebbed8c7
sebastiano210 <gordiferonieto@gmail.com>
2019-05-27 21:55:13 -0500

    hcho base en el programa

--- mobile-app/program/app/src/main/java/first/program/bank/data/model/DatabaseHelper.java
+++ mobile-app/program/app/src/main/java/first/program/bank/data/model/DatabaseHelper.java
@@ -6,17 +6,24 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-
+    private static final String TABLE_NAME= "bank";
+    private static final String COL_01="nombre ,";
+    private static final String COL_02="cedula o id";
+    private static final String COL_03="clave ,";
+    private static final String COL_04="cuenta ,";
     public DatabaseHelper(Context context){
-        super(context,bank,null);
+        super(context,TABLE_NAME,null,1);
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+            String createTable = "CREATE TABLE " + TABLE_NAME +"( "+COL_01+"cedula PRIMARY KEY,"+COL_03+COL_04+"TEXT)";
+            db.execSQL(createTable);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+         db.execSQL("DROP TABLE if EXISTS "+ TABLE_NAME);
+         onCreate(db);
     }
+    pu
 }
